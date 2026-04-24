6e99ad3d2e3efdb66ebc08b856a2f1e757897bb2
Chawder <rrorro596@gmail.com>
2026-04-23 21:20:47 -0400

    Se agrego la conexion de base de datos insumos para registrar

--- frontend/src/app/services/insumo.service.ts
+++ frontend/src/app/services/insumo.service.ts
@@ -3,12 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
+
 export class InsumoService {
-  private API_URL_CREATE = 'http://localhost:3000/api/insumos'; // POST
+
+  private API_URL_CREATE = 'http://localhost:3000/api/insumos';
 
   constructor(private http: HttpClient) {}
 
   crearInsumo(datos: any): Observable<any> {
     return this.http.post(this.API_URL_CREATE, datos);
   }
+
 }
